80c3502b58ba0abb040b14ce03a14ed510715763
anaK-Caetano <anak.94c@gmail.com>
2025-04-15 14:13:03 -0300

    feat: initial project structure - Spring Boot API skeleton - Angular client scaffold

---
+++ backend/src/test/java/caetano/ana/DesafioAmigos/DesafioAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package caetano.ana.DesafioAmigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
